f76397f8c1ee55aa8c1cfcca9cecccb65e02c961
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2025-07-23 08:54:37 -0300

    explain error handler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,7 +1,9 @@
 
 const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
+  console.error(err.stack); // err.stack = string that represents the stack trace at the point the error was created
+  // stack trace: list of the active stack frames at a certain point in time in the program's execution
   res.status(err.status || 500).json({ error: err.message || 'Internal Server Error' });
 };
 
-module.exports = errorHandler;
+module.exports = errorHandler; // makes errorHandler function available for other files to import
+
